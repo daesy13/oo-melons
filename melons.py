fe7714c42e84b377be6f717ae9124e05ed780731
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-15 17:22:01 -0700

    part 3 done

--- melons.py
+++ melons.py
@@ -11,11 +11,11 @@ class AbstractMelonOrder():
         base_price = 5
         if self.species == "Christmas":
             base_price *= 1.5
-        if self.order_type == "international" and self.qty < 10:
-            total = ((1 + self.tax) * self.qty * base_price) + 3
-            return total
 
         total = (1 + self.tax) * self.qty * base_price
+
+        if self.order_type == "international" and self.qty < 10:
+            total += 3
 
         return total
 
@@ -34,58 +34,17 @@ class DomesticMelonOrder(AbstractMelonOrder):
     order_type = "domestic"
     country_code = "USA"
 
-    # def __init__(self, species, qty):
-    #     """Initialize melon order attributes."""
-
-    #     self.species = species
-    #     self.qty = qty
-    #     self.shipped = False
-    #     self.order_type = "domestic"
-        # self.tax = 0.08
-
-    # def get_total(self):
-    #     """Calculate price, including tax."""
-
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
-
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Record the fact than an order has been shipped."""
-
-    #     self.shipped = True
-
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
     tax = 0.17
     order_type = "international"
 
-    # def __init__(self, species, qty, country_code):
-    #     """Initialize melon order attributes."""
 
-    #     self.species = species
-    #     self.qty = qty
-    #     self.country_code = country_code
-    #     self.shipped = False
-    #     self.order_type = "international"
-        # self.tax = 0.17
+class GovernmentMelonOrder(AbstractMelonOrder):
 
-    # def get_total(self):
-    #     """Calculate price, including tax."""
+    tax = 0
+    passed_inspection = False
 
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
-
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Record the fact than an order has been shipped."""
-
-    #     self.shipped = True
-
-    # def get_country_code(self):
-    #     """Return the country code."""
-
-    #     return self.country_code
+    def mark_inspection(self, passed):
+        self.passed_inspection = passed
