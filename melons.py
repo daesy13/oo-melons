5634713284d7ec50e9feb1f6bbe01ea93f2b84c5
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-15 17:03:50 -0700

    Part 2 done for melon.py

--- melons.py
+++ melons.py
@@ -1,26 +1,38 @@
 """Classes for melon orders."""
 class AbstractMelonOrder():
-	def __init__(self,species,qty,country_code = None):
-		self.species = species
-		self.qty = qty
-		self.shipped = False
-		if country_code:
-			self.country_code = country_code
+    def __init__(self,species,qty,country_code = None):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        if country_code:
+            self.country_code = country_code
 
-	def get_total(self):
-		base_price = 5
-		total = (1 + self.tax) * self.qty * base_price
+    def get_total(self):
+        base_price = 5
+        if self.species == "Christmas":
+            base_price *= 1.5
+        if self.order_type == "international" and self.qty < 10:
+            total = ((1 + self.tax) * self.qty * base_price) + 3
+            return total
 
-		return total
+        total = (1 + self.tax) * self.qty * base_price
 
-	def mark_shipped(self):
-		self.shipped = True
+        return total
+
+    def mark_shipped(self):
+        self.shipped = True
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
 
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
     tax = 0.08
     order_type = "domestic"
+    country_code = "USA"
 
     # def __init__(self, species, qty):
     #     """Initialize melon order attributes."""
@@ -73,7 +85,7 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
     #     self.shipped = True
 
-    def get_country_code(self):
-        """Return the country code."""
+    # def get_country_code(self):
+    #     """Return the country code."""
 
-        return self.country_code
+    #     return self.country_code
